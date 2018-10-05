c1e997a66b149b1e207213c57df9d66a6ae05454
huanglei <huanglei@freedotech.com>
2018-10-05 14:03:07 +0800

    Initial Commit

---
+++ miniprogram/pages/talk/talk.js
@@ -0,0 +1,39 @@
+Page({
+
+    data: {
+      array: [{
+        id: 0,
+        name: 'kinderpower清华幼儿英语',
+        qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/kinderCG.mp4',
+        qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/kinderCG.mp4.jpg',
+      }, {
+        id: 1,
+        name: 'peek-a-boo躲猫猫',
+        qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/1a_unit2_peek-a-boo.mp4',
+        qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/1a_unit2_peek-a-boo.mp4.jpg',
+      }, {
+        id: 3,
+        name: 'little_bee小蜜蜂',
+        qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/1a_unit1_little_bee.mp4',
+        qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/1a_unit1_little_bee.mp4.jpg'
+      }, {
+        id: 4,
+        name: 'Ring_Around_the_Roses玫瑰舞',
+        qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/2a_unit2_Ring_Around_the_Roses.mp4',
+        qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/2a_unit2_Ring_Around_the_Roses.mp4.jpg'
+      }, {
+        id: 5,
+        name: 'Plants植物',
+        qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/3a_unit3_Plants.mp4',
+        qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/3a_unit3_Plants.mp4.jpg'
+      }, {
+        id: 6,
+        name: 'Baby_Bumblebee蜜蜂宝贝',
+        qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/4a_unit2_Baby_Bumblebee.mp4',
+        qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/4a_unit2_Baby_Bumblebee.mp4.jpg'
+      }]
+    }
+
+  }
+
+)
---
+++ miniprogram/pages/word/word.js
@@ -0,0 +1,71 @@
+// pages/word/word.js
+Page({
+  data: {
+    array: [{
+      id: 0,
+      name: '-an',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-an.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-an.mp4.jpg',
+    }, {
+      id: 1,
+      name: '-en',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-en.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-en.mp4.jpg',
+    }, {
+      id: 2,
+      name: '-et',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-et.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-et.mp4.jpg',
+    }, {
+      id: 3,
+      name: '-ig',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-ig.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-ig.mp4.jpg',
+    }, {
+      id: 4,
+      name: '-ip',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-ip.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-ip.mp4.jpg',
+    }, {
+      id: 5,
+      name: 'my bathroom我的浴室',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mybathroom.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mybathroom.mp4.jpg',
+    }, {
+      id: 6,
+      name: 'my body我的身体',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mybody.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mybody.mp4.jpg',
+    }, {
+      id: 7,
+      name: 'my kitchen我的厨房',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mykitchen.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mykitchen.mp4.jpg',
+    }, {
+      id: 8,
+      name: 'my living room我的客厅',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mylivingroom.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mylivingroom.mp4.jpg',
+    }, {
+      id: 9,
+      name: 'What is my pet我的宠物',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/Whatismypet.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/Whatismypet.mp4.jpg',
+    }, {
+      id: 10,
+      name: 'What is my toy我的玩具',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/Whatismytoy.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/Whatismytoy.mp4.jpg',
+    }, {
+      id: 11,
+      name: 'Where do I sleep我的卧室',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/WheredoIsleep.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/WheredoIsleep.mp4.jpg',
+    }, {
+      id: 12,
+      name: 'Who am I我是谁',
+      qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/WhoamI.mp4',
+      qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/WhoamI.mp4.jpg',
+    }]
+  }
+})

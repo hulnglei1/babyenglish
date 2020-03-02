32d92ed05741d88bcf51ee8f417946e6d49df9d4
unknown <unknown@example.com>
2020-03-02 18:16:28 +0800

    0.6.7
    添加腾讯视频

---
+++ miniprogram/pages/data/data.js
@@ -0,0 +1,294 @@
+var letters_array = [{
+    id: 0,
+    name: 'A',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母A.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母A.mp4.jpg',
+    isPlay: false,
+  }, {
+    id: 1,
+    name: 'B',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母B.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母B.mp4.jpg',
+    isPlay: false,
+  }, {
+    id: 2,
+    name: 'C',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母C.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母C.mp4.jpg',
+    isPlay: false,
+  }, {
+    id: 3,
+    name: 'D',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母D.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母D.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 4,
+    name: 'E',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母E.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母E.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 5,
+    name: 'F',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母F.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母F.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 6,
+    name: 'G',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母G.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母G.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 7,
+    name: 'H',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母H.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母H.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 8,
+    name: 'I',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母I.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母I.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 9,
+    name: 'J',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母J.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母J.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 10,
+    name: 'K',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母K.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母K.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 11,
+    name: 'L',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母L.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母L.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 12,
+    name: 'M',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母M.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母M.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 13,
+    name: 'N',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母N.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母N.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 14,
+    name: 'O',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母O.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母O.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 15,
+    name: 'P',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母P.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母P.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 16,
+    name: 'Q',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母Q.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母Q.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 17,
+    name: 'R',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母R.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母R.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 18,
+    name: 'S',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母S.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母S.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 19,
+    name: 'T',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母T.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母T.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 20,
+    name: 'U',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母U.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母U.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 21,
+    name: 'V',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母V.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母V.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 22,
+    name: 'W',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母W.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母W.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 23,
+    name: 'X',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母X.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母X.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 24,
+    name: 'Y',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母Y.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母Y.mp4.jpg',
+    isPlay: false
+  }, {
+    id: 25,
+    name: 'Z',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母Z.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/字母/字母Z.mp4.jpg',
+    isPlay: false
+  }]
+
+/*
+  元音列表
+*/
+
+var vowels_array = [{
+    id: 100,
+    name: '-an',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-an.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-an.mp4_2.jpg',
+    isPlay: false,
+  }, {
+    id: 101,
+    name: '-en',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-en.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-en.mp4_2.jpg',
+    isPlay: false,
+  }, {
+    id: 102,
+    name: '-et',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-et.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-et.mp4_2.jpg',
+    isPlay: false,
+  }, {
+    id: 103,
+    name: '-ig',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-ig.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-ig.mp4_2.jpg',
+    isPlay: false,
+
+  }, {
+    id: 104,
+    name: '-ip',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-ip.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/-ip.mp4_2.jpg',
+    isPlay: false,
+  }]
+  
+/*
+  单词
+  */
+var words_array = [{
+    id: 200,
+    name: 'my bathroom我的浴室',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mybathroom.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mybathroom.mp4_2.jpg',
+    isPlay: false,
+
+  }, {
+    id: 201,
+    name: 'my body我的身体',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mybody.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mybody.mp4_2.jpg',
+    isPlay: false,
+  }, {
+    id: 202,
+    name: 'my kitchen我的厨房',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mykitchen.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mykitchen.mp4_2.jpg',
+    isPlay: false,
+  }, {
+    id: 203,
+    name: 'my living room我的客厅',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mylivingroom.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/mylivingroom.mp4_2.jpg',
+    isPlay: false,
+  }, {
+    id: 204,
+    name: 'What is my pet我的宠物',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/Whatismypet.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/Whatismypet.mp4_2.jpg',
+    isPlay: false,
+  }, {
+    id: 205,
+    name: 'What is my toy我的玩具',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/Whatismytoy.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/Whatismytoy.mp4_2.jpg',
+    isPlay: false,
+  }, {
+    id: 206,
+    name: 'Where do I sleep我的卧室',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/WheredoIsleep.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/WheredoIsleep.mp4_2.jpg',
+    isPlay: false,
+  }, {
+    id: 207,
+    name: 'Who am I我是谁',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/WhoamI.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/word/WhoamI.mp4_2.jpg',
+    isPlay: false,
+  }]
+
+
+/*
+清华幼儿英语
+*/
+var songs_array = [{
+    id: 1000,
+    name: 'kinderpower(清华幼儿英语)',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/kinderCG.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/kinderCG.mp4_2.jpg',
+  }, {
+    id: 1001,
+    name: 'peek-a-boo(躲猫猫)',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/1a_unit2_peek-a-boo.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/1a_unit2_peek-a-boo.mp4_2.jpg',
+  }, {
+    id: 1003,
+    name: 'little bee(小蜜蜂)',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/1a_unit1_little_bee.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/1a_unit1_little_bee.mp4_2.jpg'
+  }, {
+    id: 1004,
+    name: 'Ring Around the Roses',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/2a_unit2_Ring_Around_the_Roses.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/2a_unit2_Ring_Around_the_Roses.mp4_2.jpg'
+  }, {
+    id: 1005,
+    name: 'Plants（植物）',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/3a_unit3_Plants.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/3a_unit3_Plants.mp4_2.jpg'
+  }, {
+    id: 1006,
+    name: 'Baby Bumblebee（蜜蜂宝贝）',
+    qvideo: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/4a_unit2_Baby_Bumblebee.mp4',
+    qposter: 'cloud://babyenglsih-240495.6261-babyenglsih-240495/kinder/4a_unit2_Baby_Bumblebee.mp4_2.jpg'
+  }]
+
+var nickname_list = ["宝宝的大山", "宝宝的大海", "宝宝的天", "茶", "宝宝的咖啡", "夹心糖", "宝宝的书", "宝宝的严鞭", "厚墙", "登天的梯", "拉车的犁", "广阔无边的草原","参天大树","偷吃宝宝零食的小老鼠", "勤劳的小蜜蜂", "油灯", "宝宝的天使", "温和的阳光", "明亮的太阳", "参天的大树", "鼓起的航帆", "大地", "金色的灯塔", "遮风挡雨的纸伞", "闹钟", "避风的港湾", "天边的启明星"]
+
+module.exports = {
+  letters_array: letters_array,
+  vowels_array: vowels_array,
+  words_array: words_array,
+  songs_array: songs_array,
+  nickname_list: nickname_list
+}
